6bea93326e58540a04aec33ad2aecec66e5dfd93
csq95 <94054622+csq95@users.noreply.github.com>
2022-09-25 22:39:14 -0400

    Update converter.cpp

--- converter.cpp
+++ converter.cpp
@@ -1,61 +1,69 @@
 #include <iostream>
+#include<iomanip>
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+void msia(float,int);
 int main(int argc, char** argv) {
-	
 	int country;
-	float msia;
-	long amount;
-	
-	
-	do{
+	float amount;
 	cout<<"Welcome to Currency Converter!"<<endl;
-	cout<<"Please enter the number that represent the country: "<<endl;
+do{
+	cout<<"       Country Menu "<<endl;
+	cout<<"*******************************"<<endl;
 	cout<<"1. USD \n2. AUB \n3. GBP \n4. EUR \n5. RMB \n6. JPY \n7. THB \n8. SGD \n9. Exit "<<endl;
+	cout<<"Please enter the number that represent the country: "<<endl;
 	cin>>country;
-	cout<<"Please enter the amount = ";
+	cout<<"Enter Amount = ";
 	cin>>amount;
-	
 	switch (country) {
 	
 		case 1 :
-			msia=amount*4.1842313;
-			cout<<amount<<" USD = "<<msia<<" MYR"<<endl;
+			country=4.1842313;
+			msia(amount,country);
 			break;
 		case 2 :
-			msia=amount*3.0474045;
-			cout<<amount<<" AUB = "<<msia<<" MYR"<<endl;
+			country=3.0474045;
+			msia(amount,country);
 			break;
 		case 3 :
-			msia=amount*5.6862416;
-			cout<<amount<<" GBP = "<<msia<<" MYR"<<endl;
+			country=5.6862416;
+			msia(amount,country);
 			break;
 		case 4 :
-			msia=amount*4.833468;
-			cout<<amount<<" EUR = "<<msia<<" MYR"<<endl;
+			country=4.833468;
+			msia(amount,country);
 			break;
 		case 5 :
-			msia=amount*0.64850458;
-			cout<<amount<<" RMB = "<<msia<<" MYR"<<endl;
+			country=0.64850458;
+			msia(amount,country);
 			break;
 		case 6 :
-			msia=amount*0.037517282;
-			cout<<amount<<" JPY = "<<msia<<" MYR"<<endl;
+			country=0.037517282;
+			msia(amount,country);
 			break;
 		case 7 :
-			msia=amount*0.122377114;
-			cout<<amount<<" THB = "<<msia<<" MYR"<<endl;
+			country=0.122377114;
+			msia(amount,country);
 			break;
 		case 8 :
-			msia=amount*3.0785846;
-			cout<<amount<<" SGD = "<<msia<<" MYR"<<endl;
+			country=3.0785846;
+			msia(amount,country);
 			break;
+		case 9 :
+			cout<<"Have a nice day!";
 		default :
 			cout<<"Invalid number. \nPlease try again. \nThanks"; };
 	
 		} while (country!=9);
+
 		
-	
 	return 0;
+}
+
+void msia (float amount, int country ){
+	float msia;
+	msia=amount*country;
+	cout<<amount<<" $ = "<<msia<<" MYR"<<fixed<<setprecision(2);
+
 }
